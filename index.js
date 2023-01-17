f64bd5505383e8c0e23a878df767ce67a0cb5aab
Frempong <samankwaa.frempong@gmail.com>
2023-01-17 10:51:39 +0000

    Update index.js with class

--- index.js
+++ index.js
@@ -1,44 +1,62 @@
-const form = document.querySelector('.add-book-form');
-const bookTitle = document.querySelector('#book-title');
-const bookAuthor = document.querySelector('#book-author');
-const booksContainer = document.querySelector('.books-container');
+class BookList {
+  constructor() {
+    this.form = document.querySelector('.add-book-form');
+    this.bookTitle = document.querySelector('#book-title');
+    this.bookAuthor = document.querySelector('#book-author');
+    this.booksContainer = document.querySelector('.books-container');
+    this.books = JSON.parse(localStorage.getItem('Books')) || [];
+    this.render();
+    this.bindEvents();
+  }
 
-let Books = [];
+  render() {
+    this.books.forEach((book) => {
+      this.booksContainer.innerHTML += ` 
+      <div class="Books">
+                  <p class="title-text">${book.title}</p>
+                  <p class="author-text">${book.author}</p>
+                  <button class="remove-book">Remove</button>
+                  <hr>
+              </div>
+      `;
+    });
+  }
 
-if (localStorage.getItem('Books')) {
-  Books = JSON.parse(localStorage.getItem('Books'));
+  bindEvents() {
+    this.form.addEventListener('submit', this.addBook.bind(this));
+    this.booksContainer.addEventListener('click', this.removeBook.bind(this));
+  }
+
+  addBook(e) {
+    e.preventDefault();
+    this.booksContainer.innerHTML += ` 
+      <div class="Books">
+                  <p class="title-text">${this.bookTitle.value}</p>
+                  <p class="author-text">${this.bookAuthor.value}</p>
+                  <button class="remove-book">Remove</button>
+                  <hr>
+              </div>
+              
+      `;
+    this.books.push({
+      title: this.bookTitle.value,
+      author: this.bookAuthor.value,
+    });
+
+    localStorage.setItem('Books', JSON.stringify(this.books));
+  }
+
+  removeBook(e) {
+    if (e.target.classList.contains('remove-book')) {
+      const index = Array.from(
+        e.target.parentElement.parentElement.children,
+      ).indexOf(e.target.parentElement);
+      this.books.splice(index, 1);
+      e.target.parentElement.remove();
+      localStorage.setItem('Books', JSON.stringify(this.books));
+    }
+  }
 }
 
-const addBook = (e) => {
-  e.preventDefault();
-  booksContainer.innerHTML += ` 
-    <div class="Books">
-                <p class="title-text">${bookTitle.value}</p>
-                <p class="author-text">${bookAuthor.value}</p>
-                <button class="remove-book">Remove</button>
-                <hr>
-            </div>
-            
-    `;
-  Books.push({
-    title: bookTitle.value,
-    author: bookAuthor.value,
-  });
-
-  localStorage.setItem('Books', JSON.stringify(Books));
-
-  const removeBtn = document.querySelectorAll('.remove-book');
-  removeBtn.forEach((btn) => {
-    btn.addEventListener('click', (e) => {
-      if (e.target.classList.contains('remove-book')) {
-        const index = Array.from(
-          e.target.parentElement.parentElement.children,
-        ).indexOf(e.target.parentElement);
-        Books.splice(index, 1);
-        e.target.parentElement.remove();
-      }
-    });
-  });
-};
-
-form.addEventListener('submit', addBook);
+const bookList = new BookList();
+ 
